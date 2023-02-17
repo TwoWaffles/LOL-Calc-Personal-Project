b63a0bf5062786f589d212552484adb9d49141e7
TwoWaffles <damiankraszkiewicz@hotmail.com>
2023-02-17 04:39:42 +0000

    /getAllChampionNames api works but need to rename

--- lol-calc-server/src/server.js
+++ lol-calc-server/src/server.js
@@ -11,32 +11,64 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/getAllChampionNames', (req,res) => {
-    res.send({
-        champions: ["Draven", "Ashe", "Malphite"]
-    })
-});
+//Functions
+
+function filterChampionNamesAndIconsFromData(callback) {
+  fs.readFile('data/champions.json', (error,data) => {
+    if (error) throw error;
+
+    let championsArray = []
+
+    const champions = JSON.parse(data);
+    for (const champion in champions){
+      championsArray.push([champions[champion].name,champions[champion].icon]);
+    }
+
+    //console.log(championsArray)
+
+    callback(championsArray);
+    //const namesAndIcons = champions.map(champion => ({name: champion.name, icon: champion.icon}));
+
+    //console.log(namesAndIcons);
+  });
+  
+}
 
 function getChampionsJSON(){
 
-axios.get('https://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/champions.json')
-  .then(function (response) {
-    // handle success
-    fs.writeFile('data/champions.json',JSON.stringify(response.data) , function(error) {
-        if(error) throw error;
-        console.log('Saved!');
+  axios.get('https://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/champions.json')
+    .then(function (response) {
+      // handle success
+      fs.writeFile('data/champions.json',JSON.stringify(response.data) , function(error) {
+          if (error) throw error;
+          console.log('Saved!');
+      });
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
     });
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
-}
+  }
 
-getChampionsJSON();
+app.get('/getAllChampionNames', (req,res) => {
+
+    filterChampionNamesAndIconsFromData((dataToSend)=>{
+      res.send({
+          champions: dataToSend
+      })
+
+    })
+
+    
+});
+
+
+
+//getChampionsJSON();
+//filterChampionNamesAndIconsFromData();
 
 
 app.listen(process.env.PORT || 8081);
