c9963ecad8662313167e4cbeec6005769d41b0a6
TwoWaffles <damiankraszkiewicz@hotmail.com>
2023-04-20 22:30:47 +0100

    added attack speed to display

--- lol-calc-client/src/stores/ChampionOneStore.js
+++ lol-calc-client/src/stores/ChampionOneStore.js
@@ -17,12 +17,28 @@ export const useChampionOneStore = defineStore('championOneStore', {
 
     getters: {
         computedStats() {
-            const newCalculatedStats = {} 
-            for(const [key, value] of Object.entries(this.stats)){
-                        const calculatedStat = value.flat + value.perLevel * (this.level - 1) * (0.7025 + 0.0175 * (this.level - 1));
-                        newCalculatedStats[key] = calculatedStat
-                        console.log("calculating stuff")
-                    }
+            const newCalculatedStats = {}
+            for (const [key, value] of Object.entries(this.stats)) {
+                console.log("did calc")
+                let calculatedStat = 0;
+
+                switch (key) {
+                    case "attackSpeed":
+                            let attackSpeedRatio = this.stats.attackSpeedRatio.flat / value.flat
+                            let bonusAttackSpeed = value.perLevel * (this.level - 1) * (0.7025 + 0.0175 * (this.level - 1));
+                            //let extraItem = 40 + 3 + 35;
+                            //bonusAttackSpeed = bonusAttackSpeed + extraItem
+                            bonusAttackSpeed = bonusAttackSpeed * attackSpeedRatio;
+                            calculatedStat = value.flat * (1 + bonusAttackSpeed / 100 )
+                        
+                        break;
+
+                    default:
+                        calculatedStat = value.flat + value.perLevel * (this.level - 1) * (0.7025 + 0.0175 * (this.level - 1));
+
+                }
+                newCalculatedStats[key] = calculatedStat
+            }
             return newCalculatedStats
         }
     },
@@ -32,14 +48,14 @@ export const useChampionOneStore = defineStore('championOneStore', {
             const response = await getChampionDataService.getChampionData(championKey);
             //console.log(response.data);
 
-            for(const [key, value] of Object.entries(response.data)){
+            for (const [key, value] of Object.entries(response.data)) {
                 this[key] = value
             }
 
-            
+
         },
 
-        setLevel(level){
+        setLevel(level) {
             this.level = level;
         },
 
@@ -47,7 +63,7 @@ export const useChampionOneStore = defineStore('championOneStore', {
         //     console.log("calculating")
 
         //     // TODO: add cases for attack speed etc. also items
-            
+
         //     for(const [key, value] of Object.entries(this.stats)){
         //         const calculatedStat = value.flat + value.perLevel * (this.level - 1) * (0.7025 + 0.0175 * (this.level - 1));
         //         set(this.calculatedStats, key, calculatedStat);
