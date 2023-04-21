64a14973f2f892bd3040cfca0f32e78815de413c
TwoWaffles <damiankraszkiewicz@hotmail.com>
2023-04-21 20:52:23 +0100

    added basic item selection, need to implement stats

--- lol-calc-client/src/components/itemPicker/ItemInventory.vue
+++ lol-calc-client/src/components/itemPicker/ItemInventory.vue
@@ -2,53 +2,61 @@
 import ItemsService from '../../services/ItemsService';
 
 export default {
-  data() {
-    return {
-        searchQuery: '',
-        selectedItemSlot: null,
-        isVisible: false,
-        itemsArray: [],
-        inventoryArray: []
-    }
-  },
-
-  computed: {
-    filteredItems(){
-        const query = this.searchQuery.toLowerCase()
-        if(this.searchQuery = ""){
-            return this.itemsArray;
+    data() {
+        return {
+            searchQuery1: '',
+            selectedItemSlot: null,
+            isVisible: false,
+            itemsArray: [],
+            inventoryArray: [null,null,null,null,null,null]
         }
+    },
 
-        return this.itemsArray.filter((item) => {
-            const name = item[1].toLowerCase();
-            return name.startsWith(query);
-        });
-    }
-  },
+    computed: {
+        filteredItems() {
+            const query = this.searchQuery1.toLowerCase()
+            if (this.searchQuery = "") {
+                return this.itemsArray;
+            }
 
-  mounted() {
-    ItemsService.getAllItemNamesAndInfo().then(response => {
-        console.log(response.data);
-        this.itemsArray = response.data.items
-    })
-  },
+            return this.itemsArray.filter((item) => {
+                const name = item[1].toLowerCase();
+                return name.includes(query);
+            });
+        }
+    },
 
-  methods: {
-    inventorySlotOnClick(slotNumber){
-        if(this.selectedItemSlot === slotNumber){
-            this.isVisible = !this.isVisible;
+    mounted() {
+        ItemsService.getAllItemNamesAndInfo().then(response => {
+            console.log(response.data);
+            this.itemsArray = response.data.items
+        })
+    },
+
+    methods: {
+        inventorySlotOnClick(slotNumber) {
+            if (this.selectedItemSlot === slotNumber) {
+                this.isVisible = !this.isVisible;
+                this.selectedItemSlot = null;
+                return;
+            }
+
+            if (this.selectedItemSlot === null) {
+                this.isVisible = !this.isVisible;
+            }
+
+            this.selectedItemSlot = slotNumber;
+            console.log("Item slot " + slotNumber + " has been selected")
+        },
+        selectItem(item) {
+            this.inventoryArray[this.selectedItemSlot] = item;
+            this.isVisible = false;
+            console.log("Added " + item[1] + " to inventory slot " + this.selectedItemSlot);
             this.selectedItemSlot = null;
-            return;
+            //this.store get item
         }
-
-        if(this.selectedItemSlot === null){
-            this.isVisible = !this.isVisible;
-        }
-        
-        this.selectedItemSlot = slotNumber;
-        console.log("Item slot " + slotNumber + " has been selected")
     }
-  },
+
 
 
 }
@@ -57,43 +65,55 @@ export default {
 <template>
     <div>
         <div class="flex flex-row justify-between mt-2">
-            <div @click="inventorySlotOnClick(0)" class="single-item-empty" :class="[selectedItemSlot === 0 ? 'border-solid' : 'bg-blue']">
-                <font-awesome-icon icon="fa-solid fa-plus" size="2xs" />
+            <div @click="inventorySlotOnClick(0)" class="single-item-empty"
+                :class="{ 'border-solid border-yellow-500': selectedItemSlot === 0 }">
+                <font-awesome-icon v-if="inventoryArray[0] === null" icon="fa-solid fa-plus" size="2xs" />
+                <img v-else :src="inventoryArray[0][2]">
             </div>
-            <div @click="inventorySlotOnClick(1)" class="single-item-empty" :class="[selectedItemSlot === 1 ? 'border-solid' : 'bg-blue']">
-                <font-awesome-icon icon="fa-solid fa-plus" size="2xs" />
+            <div @click="inventorySlotOnClick(1)" class="single-item-empty"
+                :class="{ 'border-solid border-yellow-500': selectedItemSlot === 1 }">
+                <font-awesome-icon v-if="inventoryArray[1] === null" icon="fa-solid fa-plus" size="2xs" />
+                <img v-else :src="inventoryArray[1][2]">
             </div>
-            <div @click="inventorySlotOnClick(2)" class="single-item-empty" :class="selectedItemSlot === 2 ? 'border-solid' : 'bg-blue'">
-                <font-awesome-icon icon="fa-solid fa-plus" size="2xs" />
+            <div @click="inventorySlotOnClick(2)" class="single-item-empty"
+                :class="{ 'border-solid border-yellow-500': selectedItemSlot === 2 }">
+                <font-awesome-icon v-if="inventoryArray[2] === null" icon="fa-solid fa-plus" size="2xs" />
+                <img v-else :src="inventoryArray[2][2]">
             </div>
-            <div class="single-item-empty">
-                <font-awesome-icon icon="fa-solid fa-plus" size="2xs" />
+            <div @click="inventorySlotOnClick(3)" class="single-item-empty"
+                :class="{ 'border-solid border-yellow-500': selectedItemSlot === 3 }">
+                <font-awesome-icon v-if="inventoryArray[3] === null" icon="fa-solid fa-plus" size="2xs" />
+                <img v-else :src="inventoryArray[3][2]">
             </div>
-            <div class="single-item-empty">
-                <font-awesome-icon icon="fa-solid fa-plus" size="2xs" />
+            <div @click="inventorySlotOnClick(4)" class="single-item-empty"
+                :class="{ 'border-solid border-yellow-500': selectedItemSlot === 4 }">
+                <font-awesome-icon v-if="inventoryArray[4] === null" icon="fa-solid fa-plus" size="2xs" />
+                <img v-else :src="inventoryArray[4][2]">
             </div>
-            <div class="single-item-empty">
-                <font-awesome-icon icon="fa-solid fa-plus" size="2xs" />
+            <div @click="inventorySlotOnClick(5)" class="single-item-empty"
+                :class="{ 'border-solid border-yellow-500': selectedItemSlot === 5 }">
+                <font-awesome-icon v-if="inventoryArray[5] === null" icon="fa-solid fa-plus" size="2xs" />
+                <img v-else :src="inventoryArray[5][2]">
             </div>
         </div>
 
         <div v-show="isVisible" class="border mt-2">
             <div class="relative">
-                <input v-model="searchQuery" type="text" class="block w-full rounded-md border-gray-300 shadow-sm p-2 focus:ring-blue-500 focus:border-blue-500" placeholder="Search for an item">
+                <input v-model="searchQuery1" type="text"
+                    class="block w-full rounded-md border-gray-300 shadow-sm p-2 mb-2 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Search for an item">
             </div>
-            <div class="grid grid-cols-6 gap-2">
-                <div class="single-item">
+            <div class="grid grid-cols-6 gap-2 max-h-96 overflow-y-auto">
+                <!-- <div class="single-item">
                     <img src="https://raw.communitydragon.org/13.6/plugins/rcp-be-lol-game-data/global/default/assets/items/icons2d/1001_class_t1_bootsofspeed.png" class="border-2 border-gray-700 rounded h-14 w-14 hover:border-yellow-700">
-                </div>
-                <div v-for="(item, index) in filteredItems" :key="`item-${index}`">
-                    <img :src="item[2]" class="border-2 border-gray-700 rounded h-14 w-14 hover:border-yellow-700">
+                </div> -->
+                <div v-for="(item, index) in filteredItems" :key="`item-${index}`" class="single-item">
+                    <img @click="selectItem(item)" :src="item[2]"
+                        class="border-2 border-gray-700 rounded h-14 w-14 hover:border-yellow-500">
                 </div>
             </div>
-        </div>
-
     </div>
-</template>
 
-<style scoped>
+</div></template>
 
-</style>
+<style scoped></style>
