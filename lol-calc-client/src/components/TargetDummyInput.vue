8cfacdbbea23ff6cb597c488d96c7d00d0bbf6cf
TwoWaffles <damiankraszkiewicz@hotmail.com>
2023-04-26 04:17:43 +0100

    added input for target dummy and added values to store

--- lol-calc-client/src/components/TargetDummyInput.vue
+++ lol-calc-client/src/components/TargetDummyInput.vue
@@ -10,9 +10,9 @@ export default {
     data() {
 
         return {
-            health: championTwoStore.targetHealth,
-            armor: championTwoStore.targetArmor,
-            magicResistance: championTwoStore.targetMagicResistance,
+            health: this.championTwoStore.targetHealth,
+            armor: this.championTwoStore.targetArmor,
+            magicResistance: this.championTwoStore.targetMagicResistance,
         };
     },
     methods: {
@@ -22,10 +22,14 @@ export default {
             }
         },
         updateArmor() {
-            this.championTwoStore.setTargetArmor(this.armor);
+            if (this.armor > 0) {
+                this.championTwoStore.setTargetArmor(this.armor);
+            }
         },
         updateMagicResistance() {
-            this.championTwoStore.setTargetMagicResistance(this.magicResistance);
+            if (this.magicResistance > 0) {
+                this.championTwoStore.setTargetMagicResistance(this.magicResistance);
+            }
         },
     },
 };
@@ -35,16 +39,16 @@ export default {
     <div>
         <h1>Character Stats</h1>
         <div>
-            <label for="health">Health:</label>
-            <input type="number" id="health" v-model.number="health" @input="updateHealth" min="0" />
+            <label>Health:</label>
+            <input type="number" v-model.number="health" @input="updateHealth" />
         </div>
         <div>
-            <label for="armor">Armor:</label>
-            <input type="number" id="armor" v-model.number="armor" @input="updateArmor" />
+            <label>Armor:</label>
+            <input type="number" v-model.number="armor" @input="updateArmor" />
         </div>
         <div>
-            <label for="magicResistance">Magic Resistance:</label>
-            <input type="number" id="magicResistance" v-model.number="magicResistance" @input="updateMagicResistance" />
+            <label>Magic Resistance:</label>
+            <input type="number" v-model.number="magicResistance" @input="updateMagicResistance" />
         </div>
     </div>
 </template>
