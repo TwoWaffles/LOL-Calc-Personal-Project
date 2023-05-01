c4704fc547c39d88d930865a5db6e2b1bffd9f2d
TwoWaffles <damiankraszkiewicz@hotmail.com>
2023-05-01 20:18:03 +0100

    trying to display abilities

--- lol-calc-client/src/components/AbilityDisplay.vue
+++ lol-calc-client/src/components/AbilityDisplay.vue
@@ -20,9 +20,18 @@ export default {
 
 <template>
     <div class="flex col border-2">
+
         <!-- Passive -->
         <div>
+            <h2 class="text-white text-md text-center font-bold">Q: {{
+                getAbilities.P[0].name }}</h2>
             <img :src="getAbilities.P[0].icon" class="w-10 h-10">
+            <ul>
+                <li v-for="effect in getAbilities.P[0].effects" :key="getAbilities.P[0].effects.description" class="text-white text-xs border">
+                {{ effect.description }}
+                </li>
+            </ul>
+
         </div>
     </div>
 </template>
